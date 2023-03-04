a5f109e74853ddc88278e871900da53adf450ca0
John Trady <docktor.mom19.93@gmail.com>
2023-03-04 20:44:31 +0600

    init commit

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const submitButton = document.getElementById('submit-button');
 const prevGuesses = document.getElementById('previous-guesses');
 const lowOrHigh = document.getElementById('low-or-high');
 const resetButton = document.getElementById('reset-btn');
+const txt = document.getElementById('text');
 let randomNumber = Math.floor(Math.random() *  100) +1;
 let guessCount = 1;
 console.log(randomNumber)
@@ -32,18 +33,18 @@ const setGameOver = () => {
     console.log(randomNumber);
     resetButton.classList.remove('_visible');
     prevGuesses.classList.remove('_visible');
-    guessField.focus();
+    txt.style.display ='block';
+    // guessField.focus();
  }
 
  const chechGuess = () => {
     let userGuess = Number(guessField.value);
-    // if(guessField.value === 'e') {
-    //     submitButton.disabled = true;
-    // }
+   
     if(guessCount === 1 ) {
         prevGuesses.textContent = 'Previous guesses: ';
         prevGuesses.classList.add('_visible');
         lowOrHigh.classList.add('_visible');
+       
     }
     prevGuesses.textContent += userGuess + ' ';
 
@@ -53,7 +54,7 @@ const setGameOver = () => {
         lowOrHigh.textContent = '';
         lowOrHigh.classList.remove('_visible');
         setGameOver();
-    } else if (userGuess === 10) {
+    } else if (guessCount === 10) {
         lastResult.textContent = '!!!GAME OVER!!!';
         lowOrHigh.classList.remove('_visible');
         setGameOver();
@@ -83,4 +84,9 @@ const setGameOver = () => {
     }
 
     
+ });
+
+ guessField.addEventListener('focus', e => {
+    txt.style.display= 'none';
+   
  })
